bede8477599f98492eecc90d2416dc79f1e13dc5
unknown <Daniel@.(none)>
2012-03-26 00:37:19 +0200

    ajout element inscription entreprise

--- Serveur/web/entretien/php/inscription.php
+++ Serveur/web/entretien/php/inscription.php
@@ -43,6 +43,12 @@
 		  <input class="input-medium" type="text" id="nomEntreprise"/>
 		</div>
 	  </div>
+	  <div class="control-group" id="control_villeEntreprise">
+		<label class="control-label">Ville</label>
+		<div class="controls">
+		  <input class="input-medium" type="text" id="villeEntreprise"/>
+		</div>
+	  </div>
 	  <div class="control-group" id="control_date">
 		<label class="control-label">Date</label>
 		<div class="controls">
@@ -55,7 +61,6 @@
 		<div class="controls">
 		  <select id="heureDebut" class="input-small">
 		  <option>choix</option>
-			<option>13h</option>
 			<option>14h</option>
 			<option>15h</option>
 			<option>16h</option>
@@ -77,7 +82,6 @@
 			<option>15h</option>
 			<option>16h</option>
 			<option>17h</option>
-			<option>18h</option>
 		  </select>
 		  <select id="minuteFin" class="input-small">
 			<option>00</option>
@@ -87,7 +91,26 @@
 		  </select>
 		</div>
 	  </div>
-	  
+
+	<!-- Partie relative au intervenants -->
+	<table class="table table-striped">
+	<thead>
+	  <tr>
+		<th>Nom</th>
+		<th>Prenom</th>
+		<th>E Mail</th>
+	  </tr>
+	</thead>
+	<tbody>
+	<td>Le Roux</td>
+	<td>Bill</td>
+	<td>bill.le.roux@laposte.fr</td>
+	</tbody>
+	</table>
+	<a class="offset7 actuator btn btn-primary" data-toggle="modal" href="#myModal">
+	Ajouter Intervenant
+	</a>
+	
 	  <div class="form-actions">
 		<button type="submit" class="btn btn-primary">Valider</button>
 		<button class="btn">Effacer</button>
@@ -95,6 +118,44 @@
 	</fieldset>
   </form>
 
+
+<div class="modal hide fade" id="myModal">
+	<div class="modal-header">
+	<a class="close" data-dismiss="modal">×</a>
+	<h3>Entretien</h3>
+	</div>
+	<div class="modal-body">
+	   <form class="form-horizontal" method="post" action="#">
+			<div class="control-group" id="control_nom">
+			<label class="control-label">Nom</label>
+			<div class="controls">
+			  <input class="input-medium" type="text" id="nom"/>
+			</div>
+		  </div>
+		  <div class="control-group" id="control_prenom">
+			<label class="control-label">Prenom</label>
+			<div class="controls">
+			  <input class="input-medium" type="text" id="prenom"/>
+			</div>
+		  </div>
+		  <div class="control-group" id="control_email">
+			<label class="control-label" for="email">E mail</label>
+			<div class="controls">
+			  <div class="input-prepend">
+				<span class="add-on">@</span><input class="input-medium" id="email" type="text">
+			  </div>
+			</div>
+		  </div>
+		  
+			<div class="modal-footer">
+			<input type="submit" class="btn btn-primary" value="Valider"/>
+			<a href="#" class="btn" data-dismiss="modal">Annuler</a>
+			</div>
+		</form>
+	</div>
+</div>
+  
+  
 <!--[if lt IE 7]><script type="text/javascript" src="scripts/jquery.bgiframe.min.js"></script><![endif]-->
 
 <script type="text/javascript" charset="utf-8">
