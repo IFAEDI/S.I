c828b9da9fe4ac74bd9c4c902d1030e4b288c89c
PandiPanda <thedestiny@SNuX-Vostro-3550.(none)>
2012-03-31 02:58:22 +0200

    [Auth] Remaniement de la base OK pour les utilisateurs normaux (non CAS)

---
+++ Serveur/web/commun/php/personne.class.php
@@ -0,0 +1,279 @@
+<?php
+
+require_once dirname(__FILE__) . '/base.inc.php';
+inclure_fichier('commun', 'bd.inc', 'php');
+
+class Personne {
+
+	/* Constantes liées aux personnes */
+	const ETUDIANT		= 0;
+	const ENSEIGNANT	= 1;
+	const ENTREPRISE	= 2;
+	const ADMIN		= 3;
+	const AEDI		= 4;
+
+	public static $ROLES = array( self::ETUDIANT => 'Etudiant',
+				self::ENSEIGNANT => 'Enseignant',
+				self::ENTREPRISE => 'Entreprise',
+				self::ADMIN => 'Administrateur',
+				self::AEDI => 'Membre de l\'AEDI' );
+
+	/****************  Attributs  ******************/
+	private $id;
+	private $nom;
+	private $prenom;
+	private $mails;
+	private $telephones;
+	private $premiereConnexion;
+	private $role;
+
+	private $utilisateur;
+
+
+	/**
+	* Constructeur
+	* $utilisateur : Objet Utilisateur auquel est rattachée la personne
+	* @throws Exception si impossibilité de créer la personne
+	*/
+	public function __construct( $utilisateur ) {
+        
+		$this->utilisateur = $utilisateur;
+
+		$result = $this->_fetchData( $this->utilisateur->getId() );
+
+		/* Si l'objet n'a pas pu être créé, c'est sans doute que c'est une auth via le CAS et que l'user est pas en base */
+		if( $result == false ) {
+
+			/* On s'en occupe donc ! */
+			/* Ajout en base */
+			$result = BD::executeModif( 'INSERT INTO UTILISATEUR(login, nom, service, premiere_connexion) VALUES( :login, :nom, :service, 1 )', array( 'login' => $login, 'nom' => $login, 'service' => Authentification::AUTH_CAS ) );
+
+			if( $result == 0 ) {
+				throw new Exception( 'Impossible d\'insérer le nouvel utilisateur en base.' );
+			} 
+
+			/* Et on rappelle pour fetcher les éléments */
+			$result = $this->_fetchData( $login );
+			if( $result == false ) {
+				throw new Exception( 'Impossible de construire l\'utilisateur (erreur de bdd).' );
+			}
+		}
+	}
+
+	/**
+	* Fonction récupérant tous les attributs de la personne
+	* $id : Identifiant de l'utilisateur rattachée à la personne à qui on doit récupérer les données
+	* @return True si tout est ok, false sinon
+	*/
+	private function _fetchData( $id ) {
+
+		/* Requête à la base pour récupérer la bonne personne et construire l'objet */
+		$result = BD::executeSelect( 'SELECT * FROM PERSONNE WHERE ID_UTILISATEUR = :id', array( 'id' => $id ), BD::RECUPERER_UNE_LIGNE );
+
+		if( $result == null )
+			return false;
+
+		$this->id = $result['ID_PERSONNE'];
+		$this->nom = $result['NOM'];
+		$this->prenom = $result['PRENOM'];
+		$this->role = $result['ROLE'];
+		$this->premiereConnexion = $result['PREMIERE_CONNEXION'];
+
+		/* Récupération des adresses mails associées */
+		$result = BD::executeSelect( 'SELECT * FROM MAIL WHERE ID_PERSONNE = :id ORDER BY PRIORITE',
+					array( 'id' => $this->id ), BD::RECUPERER_TOUT );
+
+		$this->mails = array();
+		if( $result != null ) {
+
+			$i = 0;
+			foreach( $result as $row ) {
+				$this->mails[$i] = array( $row['INTITULE'], $row['MAIL'] );
+				$i++;
+			}
+		}
+
+		/* Récupération des numéros de tél associés */
+		$result = BD::executeSelect( 'SELECT * FROM TELEPHONE WHERE ID_PERSONNE = :id ORDER BY PRIORITE',
+					array( 'id' => $this->id ), BD::RECUPERER_TOUT );
+
+		$this->telephones = array();
+		if( $result != null ) {
+
+                        $i = 0;
+                        foreach( $result as $row ) {
+                                $this->telephones[$i] = array( $row['INTITULE'], $row['NUMERO'] );
+                                $i++;
+                        }
+		}
+
+		return true;
+	}
+
+	/**
+	* Change les informations personnelles de l'utilisateur
+	* @return Vrai si tout est ok, faux sinon
+	*/
+	public function changeInfo( $nom, $prenom ) {
+
+		/* Requête à la base */
+		$result = BD::executeModif( 'UPDATE PERSONNE SET nom = :nom, prenom = :prenom, premiere_connexion = 0 WHERE ID_PERSONNE = :id',
+			array( 'nom' => $nom, 'prenom' => $prenom, 'id' => $this->id ) );
+
+		if( $result == 0 ) {
+			return false;
+		}
+
+		$this->nom = $nom;
+		$this->prenom = $prenom;
+		$this->premiereConnexion = false;
+
+		return true;
+	}
+
+	/**
+	* Met à jour les adresses mails de la personne
+	* $mails : Un tableau contenant tous les mails de la forme { { Libellé, Mail }, { Libellé, Mail } }
+	* @return Vrai si tout est ok, faux sinon
+	*/
+	public function changeMails( $mails ) {
+
+		/* Vide la base pour ajouter les nouveaux mails */
+		BD::executeModif( 'DELETE FROM MAIL WHERE ID_PERSONNE = :id', array( 'id' => $this->id ) );
+		$this->mails = array();
+
+		$i = 0;
+		foreach( $mails as $mail ) {
+			if( strlen( $mail[1] ) > 0 ) {
+
+				$libelle  = strip_tags( mysql_escape_string( $mail[0] ) );
+				$cur_mail = strip_tags( mysql_escape_string( $mail[1] ) );
+
+				$result = BD::executeModif( 'INSERT INTO MAIL VALUES( :id, :libelle, :mail, :priorite )', 
+					array( 'id' => $this->id, 'libelle' => $libelle, 'mail' => $cur_mail, 'priorite' => $i ) );
+
+				if( $result == 0 ) {
+					return false;
+				}
+
+				$this->mails[$i] = array( $libelle, $cur_mail );
+
+				$i++;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	* Met à jour les numéros de téléphone de la personne
+	* $telephones : Un tableau contenant tous les numéros de la forme { { Libellé, Tel }, { Libellé, Tel } }
+	* @return Vrai si tout est ok, faux sinon
+	*/
+	public function changeTelephones( $telephones ) {
+
+                /* Vide la base pour ajouter les nouveaux mails */
+                BD::executeModif( 'DELETE FROM TELEPHONE WHERE ID_PERSONNE = :id', array( 'id' => $this->id ) );
+		$this->telephones = array();
+
+                $i = 0;
+                foreach( $telephones as $tel ) {
+                        if( strlen( $tel[1] ) > 0 ) {
+
+                                $libelle  = strip_tags( mysql_escape_string( $tel[0] ) );
+                                $cur_tel  = strip_tags( mysql_escape_string( $tel[1] ) );
+
+                                $result = BD::executeModif( 'INSERT INTO TELEPHONE VALUES( :id, :libelle, :tel, :priorite )',
+                                        array( 'id' => $this->id, 'libelle' => $libelle, 'tel' => $cur_tel, 'priorite' => $i ) );
+
+                                if( $result == 0 ) {
+                                        return false;
+                                }
+
+                                $this->telephones[$i] = array( $libelle, $cur_tel );
+
+                                $i++;
+                        }
+                }
+
+                return true;
+	}
+
+	/**
+	* Change le type d'utilisateur
+	* @return Vrai si tout est ok, faux sinon
+	*/
+	public function changeUtilisateurType( $type ) {
+
+		/* Requête de mise à jour */
+		$result = BD::executeModif( 'UPDATE UTILISATEUR SET type = :type WHERE id = :id', array( 'type' => $type, 'id' => $this->id ) );
+
+		if( $result == 0 ) {
+			return false;
+		}
+
+		$this->typeUtilisateur = $type;
+
+		return true;
+	}
+
+
+	/**
+	* Détermine si c'est la première connexion de l'utilisateur ou non
+	* @return Vrai si c'est le cas, faux sinon
+	*/
+	public function premiereConnexion() {
+		return $this->premiereConnexion;
+	}
+
+	/**
+	* Retourne le role de la personne
+	*/
+	public function getRole() {
+		return $this->role;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getNom() {
+		return $this->nom;
+	}
+
+	public function getPrenom() {
+		return $this->prenom;
+	}
+
+	public function getMails() {
+		return $this->mails;
+	}
+
+	public function getTelephones() {
+		return $this->telephones;
+	}
+
+	/**
+	* Récupère l'ensemble des utilisateurs en base
+	* @return Un tableau d'instances
+	* @throws Une exception en cas d'erreur
+	*/
+	public static function RecupererTousLesUtilisateurs() {
+
+		$obj = array();
+
+		/* Requête à la base pour récupérer les logins et construire les objets */
+		$result = BD::executeSelect( 'SELECT login FROM UTILISATEUR', array(), BD::RECUPERER_TOUT );
+
+		$i = 0;
+		foreach( $result as $row ) {
+
+			$obj[$i] = new Utilisateur( $row['login'] );
+			$i++;
+		}
+
+		return $obj;
+	}
+}
+
+?>
--- Serveur/web/commun/php/utilisateur.class.php
+++ Serveur/web/commun/php/utilisateur.class.php
@@ -1,34 +1,17 @@
 <?php
 
 require_once dirname(__FILE__) . '/base.inc.php';
-inclure_fichier('commun', 'bd.inc', 'php');
+inclure_fichier('commun', 'personne.class', 'php');
 
 class Utilisateur {
-
-	/* Constantes liées aux utilisateurs */
-	const UTILISATEUR_ETUDIANT	= 0;
-	const UTILISATEUR_ENSEIGNANT	= 1;
-	const UTILISATEUR_ENTREPRISE	= 2;
-	const UTILISATEUR_ADMIN		= 3;
-	const UTILISATEUR_AEDI		= 4;
-
-	public static $UTILISATEUR_TYPES = array( self::UTILISATEUR_ETUDIANT => 'Etudiant',
-				self::UTILISATEUR_ENSEIGNANT => 'Enseignant',
-				self::UTILISATEUR_ENTREPRISE => 'Entreprise',
-				self::UTILISATEUR_ADMIN => 'Administrateur',
-				self::UTILISATEUR_AEDI => 'Membre de l\'AEDI' );
 
 	/****************  Attributs  ******************/
 	private $id;
 	private $login;
-	private $nom;
-	private $prenom;
-	private $annee;
-	private $mail;
 	private $service;
-	private $premiereConnexion;
-	private $typeUtilisateur;
+	private $banni;
 
+	private $personne;
 
 	/**
 	* Constructeur
@@ -41,6 +24,8 @@ class Utilisateur {
 
 		/* Si l'objet n'a pas pu être créé, c'est sans doute que c'est une auth via le CAS et que l'user est pas en base */
 		if( $result == false ) {
+
+			echo "pouet";
 
 			/* On s'en occupe donc ! */
 			/* Ajout en base */
@@ -56,6 +41,9 @@ class Utilisateur {
 				throw new Exception( 'Impossible de construire l\'utilisateur (erreur de bdd).' );
 			}
 		}
+
+		/* Et maintenant on crée l'objet Personne associé */
+		$this->personne = new Personne( $this );
 	}
 
 	/**
@@ -68,18 +56,14 @@ class Utilisateur {
 		/* Requête à la base pour récupérer le bon utilisateur et construire l'objet */
 		$result = BD::executeSelect( 'SELECT * FROM UTILISATEUR WHERE login = :login', array( 'login' => $login ), BD::RECUPERER_UNE_LIGNE );
 
-		if( $result == null )
+		if( $result == null ) {
 			return false;
+		}
 
-		$this->id = $result['id'];
-		$this->login = $result['login'];
-		$this->nom = $result['nom'];
-		$this->prenom = $result['prenom'];
-		$this->annee = $result['annee'];
-		$this->mail = $result['mail'];
-		$this->service = $result['service'];
-		$this->premiereConnexion = $result['premiere_connexion'];
-		$this->typeUtilisateur = $result['type'];
+		$this->id = $result['ID_UTILISATEUR'];
+		$this->login = $result['LOGIN'];
+		$this->service = $result['AUTH_SERVICE'];
+		$this->banni = $result['BANNI'];
 
 		return true;
 	}
@@ -91,67 +75,18 @@ class Utilisateur {
 	public function changePassword( $mdp ) {
 
 		/* Requête à la base */
-		$result = BD::executeModif( 'UPDATE UTILISATEUR SET mdp = :mdp WHERE id = :id', array( 'mdp' => $mdp, 'id' => $this->id ) );
+		$result = BD::executeModif( 'UPDATE UTILISATEUR SET PASSWD = :mdp WHERE ID_UTILISATEUR = :id', 
+							array( 'mdp' => $mdp, 'id' => $this->id ) );
 
 		return ($result == 1);
 	}
 
-	/**
-	* Change les informations personnelles de l'utilisateur
-	* @return Vrai si tout est ok, faux sinon
-	*/
-	public function changeInfoPerso( $nom, $prenom, $mail, $annee ) {
-
-		/* Requête à la base */
-		$result = BD::executeModif( 'UPDATE UTILISATEUR SET nom = :nom, prenom = :prenom, annee = :annee, mail = :mail, premiere_connexion = 0 WHERE id = :id',
-			array( 'nom' => $nom, 'prenom' => $prenom, 'annee' => $annee, 'mail' => $mail, 'id' => $this->id ) );
-
-		if( $result == 0 ) {
-			return false;
-		}
-
-		$this->nom = $nom;
-		$this->prenom = $prenom;
-		$this->annee = $annee;
-		$this->mail = $mail;
-		$this->premiereConnexion = false;
-
-		return true;
-	}
 
 	/**
-	* Change le type d'utilisateur
-	* @return Vrai si tout est ok, faux sinon
+	* Retourne la personne associée à l'utilisateur
 	*/
-	public function changeUtilisateurType( $type ) {
-
-		/* Requête de mise à jour */
-		$result = BD::executeModif( 'UPDATE UTILISATEUR SET type = :type WHERE id = :id', array( 'type' => $type, 'id' => $this->id ) );
-
-		if( $result == 0 ) {
-			return false;
-		}
-
-		$this->typeUtilisateur = $type;
-
-		return true;
-	}
-
-
-	/**
-	* Détermine si c'est la première connexion de l'utilisateur ou non
-	* @return Vrai si c'est le cas, faux sinon
-	*/
-	public function premiereConnexion() {
-		return $this->premiereConnexion;
-	}
-
-	/**
-	* Retourne le type d'utilisateur sous forme d'entier
-	*/
-	public function getTypeUtilisateur() {
-
-		return $this->typeUtilisateur;
+	public function getPersonne() {
+		return $this->personne;
 	}
 
 	/**
@@ -161,26 +96,16 @@ class Utilisateur {
 		return $this->login;
 	}
 
+	/**
+	* Retourne l'identifiant de l'utilisateur
+	*/
 	public function getId() {
 		return $this->id;
 	}
 
-	public function getNom() {
-		return $this->nom;
-	}
-
-	public function getPrenom() {
-		return $this->prenom;
-	}
-
-	public function getAnnee() {
-		return $this->annee;
-	}
-
-	public function getMail() {
-		return $this->mail;
-	}
-
+	/**
+	* Retourne le type d'authentification
+	*/
 	public function getService() {
 		return $this->service;
 	}
