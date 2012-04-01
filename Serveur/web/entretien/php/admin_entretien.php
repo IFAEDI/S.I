cf597393f2aa13ddcbbf5898ea8c3a4587ab513b
unknown <Daniel@.(none)>
2012-04-01 20:43:45 +0200

    ajout fonction partie admin

--- Serveur/web/entretien/php/admin_entretien.php
+++ Serveur/web/entretien/php/admin_entretien.php
@@ -18,7 +18,7 @@ Vous pouvez accepter ou refuser celles qui ne sont pas encore inscrite.
 
 <p> Si vous souhaitez ajouter manuellement une entreprise, utilisez le boutton ci-dessous afin de renseigner les informations necessaires. 
 </p>
-<a class="reservation btn btn-primary offset5" data-toggle="modal" href="#myModal">Ajouter Entretien</a>
+<a class="reservation btn btn-primary offset5" href="index.php?page=inscription">Ajouter Entretien</a>
 
 
 <br />
@@ -47,7 +47,7 @@ Vous pouvez accepter ou refuser celles qui ne sont pas encore inscrite.
 </div>
 
 
-<div class="modal hide fade" id="myModal">
+<div class="modal hide fade" id="accepterModal">
 	<div class="modal-header">
 	<a class="close" data-dismiss="modal">×</a>
 	<h3>Entretien</h3>
@@ -55,7 +55,7 @@ Vous pouvez accepter ou refuser celles qui ne sont pas encore inscrite.
 	<div class="modal-body">
 	   <form class="form-horizontal" id="formValiderEntretien" method="post" action="#" >
 			<input type="hidden" id="id_entretien"/>
-			<p>Etes-vous sur de vouloir vous valider cet entretien ?</p>
+			<p>Etes-vous sur de vouloir valider cet entretien ?</p>
 			<div class="modal-footer">
 			<input type="submit" class="btn btn-primary" value="Valider"/>
 			<a href="#" class="btn" data-dismiss="modal">Annuler</a>
@@ -65,8 +65,67 @@ Vous pouvez accepter ou refuser celles qui ne sont pas encore inscrite.
 </div>
 
 
+<div class="modal hide fade" id="refuserModal">
+	<div class="modal-header">
+	<a class="close" data-dismiss="modal">×</a>
+	<h3>Entretien</h3>
+	</div>
+	<div class="modal-body">
+	   <form class="form-horizontal" id="formRefuserEntretien" method="post" action="#" >
+			<input type="hidden" id="id_entretien"/>
+			<p>Etes-vous sur de vouloir annuler cet entretien ?</p>
+			<div class="modal-footer">
+			<input type="submit" class="btn btn-primary" value="Valider"/>
+			<a href="#" class="btn" data-dismiss="modal">Annuler</a>
+			</div>
+		</form>
+	</div>
+</div>
+
+
+<div class="modal hide fade" id="ajouterEtudiantModal">
+	<div class="modal-header">
+	<a class="close" data-dismiss="modal">×</a>
+	<h3>Entretien</h3>
+	</div>
+	<div class="modal-body">
+	   <form class="form-horizontal" id="formAjoutEtudiant" method="post" action="#" >
+			<input type="hidden" id="id_creneau"/>
+			<p>Etes-vous sur de vouloir inscrire un etudiant a ce creneau ?</p>
+			<div class="modal-footer">
+			<input type="submit" class="btn btn-primary" value="Valider"/>
+			<a href="#" class="btn" data-dismiss="modal">Annuler</a>
+			</div>
+		</form>
+	</div>
+</div>
+
+
+<div class="modal hide fade" id="refuserEtudiantModal">
+	<div class="modal-header">
+	<a class="close" data-dismiss="modal">×</a>
+	<h3>Entretien</h3>
+	</div>
+	<div class="modal-body">
+	   <form class="form-horizontal" id="formAnnulation" method="post" action="#" >
+			<input type="hidden" id="id_creneau"/>
+			<p>Etes-vous sur de vouloir desinscrire cet etudiant ?</p>
+			<div class="modal-footer">
+			<input type="submit" class="btn btn-primary" value="Valider"/>
+			<a href="#" class="btn" data-dismiss="modal">Annuler</a>
+			</div>
+		</form>
+	</div>
+</div>
+
+
+
 <script type="text/javascript" charset="utf-8">
 
+// Permet d'afficher le choix de la date
+$(function(){
+	$('.date-pick').datePicker();
+});
 
 </script>
 
