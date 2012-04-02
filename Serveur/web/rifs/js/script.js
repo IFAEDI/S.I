a1846169ad7bec8cdb9cff57bd2670554fa7eba2
Benjamin Planche <benjamin.planche@aldream.net>
2012-04-02 03:47:56 +0200

    <Merge> Restauration RIF.

--- Serveur/web/rifs/js/script.js
+++ Serveur/web/rifs/js/script.js
@@ -1,89 +1,163 @@
 /*---------------------------------------------------------------------------------
-					PARTIE JAVASCRIPT
----------------------------------------------------------------------------------*/		
+PARTIE JAVASCRIPT
+---------------------------------------------------------------------------------*/
 function changementTypeEntreprise(champ){
 	if (champ.options[champ.selectedIndex].value == "autre" && $("#autreTypeEntreprise").css("display") == "none")
-		$("#autreTypeEntreprise").show("slow");
+	$("#autreTypeEntreprise").show("slow");
 	else
-		$("#autreTypeEntreprise").hide("slow");
-}
+	$("#autreTypeEntreprise").hide("slow");
+	}
 
-function valider(){
-	  var valide = true;
-	  //On test la valeur des champs du formulaire
+	function ajouterIntervenant(){
+	if ($('.nomPrenomResponsable').length < 8){
+	$('#control_participants').append($('.nomPrenomIntervenant').last().clone());
+	$('.nomPrenomIntervenant').last().children('.nomIntervenant').val('');
+	$('.nomPrenomIntervenant').last().children('.prenomIntervenant').val('');
+	if ($('.nomPrenomResponsable').length == 8){
+	// TODO : Mettre un label pour indiquer que l'on ne peut plus ajouter d'intervenants
+	}
+	}
+	return false;
+	}
 
-	  // NOM DE L ENTREPRISE
-	  if(document.formInscription.nomEntreprise.value == ""){
-		  var div = document.getElementById("control_nomEntreprise");
-		  div.className ="control-group error";
-		  valide = false;
-	  }
+	function valider(){
+	var valide = true;
+	//On test la valeur des champs du formulaire
+
+	// NOM DE L ENTREPRISE
+	if(document.formInscription.nomEntreprise.value == ""){
+		var div = document.getElementById("control_nomEntreprise");
+		div.className ="control-group error";
+		valide = false;
+	}
 
 	// NOM DU RESP.
-	  if(document.formInscription.nomResponsable.value == ""){
+	if(document.formInscription.nomResponsable.value == ""){
 		var div = document.getElementById("control_nomResponsable");
 		div.className ="control-group error";
 		valide = false;
-	  }
-	  
+	}
+
 	// PRENOM DU RESP.
 	if(document.formInscription.prenomResponsable.value == ""){
 		var div = document.getElementById("control_nomResponsable");
 		div.className ="control-group error";
 		valide = false;
 	}
-	  
-	  // TEL DU RESP.
-	  if(document.formInscription.telephone.value == ""){
+
+	// TEL DU RESP.
+	if(document.formInscription.telephone.value == ""){
 		var div = document.getElementById("control_tel");
 		div.className ="control-group error";
 		valide = false;
-	  }
-	  
-	  // EMAIL
-	  if(document.formInscription.mail.value != ""
-	  && verifMail(document.formInscription.mail) ){
+	}
+
+	// EMAIL
+	if(document.formInscription.mail.value != ""
+		&& verifMail(document.formInscription.mail) ){
 		var div = document.getElementById("control_mail");
 		div.className ="control-group success";
-	  }else{
+	}else{
 		var div = document.getElementById("control_mail");
 		div.className ="control-group error";
 		valide = false;
-	  }
-
-
-	  // DESCRIPTIF DE L ENTREPRISE
-	  if(document.formInscription.descriptionEntreprise.length != null){
-		var div = document.getElementById("control_descEntreprise");
-		div.className ="control-group success";
-	  }else{
-		  var div = document.getElementById("control_descEntreprise");
-		  div.className ="control-group error";
-		  valide = false;
-	  }
-	  
-	  var retour = (valide == true ? true : false);	  
-	  return retour;
 	}
 
+	// TODO : vérifier les intervenants
+
+	// DESCRIPTIF DE L ENTREPRISE
+	if(document.formInscription.descriptionEntreprise.value != ""){
+		var div = document.getElementById("control_descEntreprise");
+		div.className ="control-group success";
+	}else{
+		var div = document.getElementById("control_descEntreprise");
+		div.className ="control-group error";
+		valide = false;
+	}
+
+	var retour = (valide == true ? true : false);
+	return retour;
+}
+
+
+// Permet de verifier l'email
+function verifMail(champ)
+{
+	var regex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if(!regex.test(champ.value) )
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
 /*---------------------------------------------------------------------------------
-					PARTIE AJAX
+PARTIE AJAX
 ---------------------------------------------------------------------------------*/
 function soumettreFormulaire(){
 	var result = valider();
 	if (result == true){
-		var nomEntreprise = document.formInscription.nomEntreprise.value;
+		var nomIntervenants = new Array();
+		$('.nomIntervenant').each(function(index) {
+			if (this.value != '')
+				nomIntervenants[index] = this.value;
+		});
+		var prenomIntervenants = new Array();
+		$('.prenomIntervenant').each(function(index) {
+			if (this.value != '')
+				prenomIntervenants[index] = this.value;
+		});
+		var typeEntreprise = (document.formInscription.typeEntreprise.value == "autre")
+			? document.formInscription.typeEntreprise.value:document.formInscription.typeEntrepriseAutre.value;
+
+		for (var i=0; i < document.formInscription.momentPresence.length; i++)
+		{
+			if (document.formInscription.momentPresence[i].checked) {var momentPresenceVal = document.formInscription.momentPresence[i].value;}
+		}
+
+		var nbIntervRestaurant = 0;
+		for (var i=0; i < document.formInscription.restaurant.length; i++)
+		{
+			if (document.formInscription.restaurant[i].checked)
+			{
+				var restaurantVal = document.formInscription.restaurant[i].value;
+				if (restaurantVal == 'oui')
+					nbIntervRestaurant = document.formInscription.nomIntervenant.length;
+			}
+		}
+
+		for (var i=0; i < document.formInscription.TA.length; i++)
+		{
+			if (document.formInscription.TA[i].checked)
+				{var TA = document.formInscription.TA[i].value;}
+		}
+
 		$.post("rifs/ajax/inscription_post.cible.php",
-		{ prenom : "prenom"},
+		{
+			nomEntreprise : document.formInscription.nomEntreprise.value,
+			nomResponsable : document.formInscription.nomResponsable.value,
+			prenomResponsable : document.formInscription.prenomResponsable.value,
+			telephone : document.formInscription.telephone.value,
+			mail : document.formInscription.mail.value,
+			typeEntreprise: typeEntreprise,
+			nomIntervenants : nomIntervenants,
+			prenomIntervenants : prenomIntervenants,
+			momentPresence : momentPresenceVal,
+			restaurant : restaurantVal,
+			nbPers_restaurant : nbIntervRestaurant,
+			TA : TA,
+			infoMatosTechnique : document.formInscription.infoMatosTechnique.value,
+			infoNbPrise : document.formInscription.infoNbPrise.value,
+			attente : document.formInscription.attente.value,
+			descriptionEntreprise : document.formInscription.descriptionEntreprise.value,
+			autre : document.formInscription.autre.value,
+		},
 		function success(retour){
-				if(retour == "1" ){
-					alert('coucou');
-				}
-			});
-	}else
-	{
-		window.scrollTo(0,0);
-		return false;
+			$(".module").html(retour);
+		});
 	}
-	
+	window.scrollTo(0,0);
+	return false;
 }
