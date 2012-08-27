95a7c361ea6512163d6d3d8f52ff14d57618ccf5
Aldream <benjamin.planche@aldream.net>
2012-08-27 21:21:47 +0200

    MaJ Module RIF (design, contenu)
    Design et texte modifiés, formulaire non accessible au public (à
    discuter), brève administration pour compenser.

--- Serveur/web/rifs/php/rifs.php
+++ Serveur/web/rifs/php/rifs.php
@@ -1,4 +1,4 @@
-<div id="rifs">
+<div id="rifs" class="module_entr">
 	<div class="row">
 		<div id="presentationRif" class="span6">
 			<div class="hero-unit">
@@ -7,43 +7,19 @@
 					Les <span class="hero_motcle">Rencontres IF</span>, ou <span class="hero_motcle">RIFs</span>, sont une manifestation propre au Département Informatique de l'INSA de Lyon. Elles permettent, le temps d'une journée, d'élaborer un lien privilégié entre les entreprises et les étudiants.
 				</p>
 				<p>
-					Fort de son succès depuis plus de <strong>dix ans</strong> cet évènement se renouvelle chaque année en recherchant toujours plus de qualité dans sa prestation.
+					Fort de son succès depuis plus de <strong>dix ans</strong>, cet évènement se renouvelle chaque année en recherchant toujours plus de qualité dans sa prestation.
 				</p>
 			</div>
 		</div>
-		<div id="myCarousel" class="carousel slide span6">
-			<div class="carousel-inner">
-				<div class="item active">
-					<img src="rifs/img/DSC04323.jpg" width="770px" alt="Rencontres IF - photo">
-					<div class="carousel-caption">
-					<h4>Un entretien</h4>
-					<p></p>
-					</div>
-				</div>
-				<div class="item">
-					<img src="rifs/img/DSC04353.jpg" width="770px" alt="Rencontres IF - Ambiance détendue">
-					<div class="carousel-caption">
-					<h4>Dans une ambiance détendue</h4>
-					<p>Parce que cet évènement ne veut pas se faire dans une ambiance rigide !</p>
-					</div>
-				</div>
-				<div class="item">
-					<img src="rifs/img/DSC04386.jpg" width="770px" alt="Une salle avec toutes ses entreprises">
-					<div class="carousel-caption">
-					<h4>Les discussions vont bon train!</h4>
-					<p></p>
-					</div>
-				</div>
-			</div>
-			<a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-			<a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
-		</div>
+		<p class="slide span6">
+			<img src="rifs/img/rifs_salle.jpg" alt="Une salle avec toutes ses entreprises">
+		</p>
 	</div>
 	<div class="row">
 		<div id="conceptRif" class="span4">
-			<h2>Concept</h2>
+			<h2><i class="icon icon-bookmark icon-white"></i> Concept</h2>
 			<p>
-				<strong>Plus d'une vingtaine d'entreprises</strong> est invitée au sein du département afin de venir à la <strong>rencontre des étudiants</strong>. Celles-ci y sont accueillies par une équipe composée exclusivement d'étudiants, et la journée, banalisée pour l'évènement, permettra à chacun de venir se renseigner, poser des questions, prendre des contacts, ...
+				<strong>Plus d'une vingtaine d'entreprises</strong> sont invitées au sein du département afin de venir à la <strong>rencontre des étudiants</strong>. Celles-ci y sont accueillies par une équipe composée exclusivement d'étudiants, et la journée, banalisée pour l'évènement, permettra à chacun de venir se renseigner, poser des questions, prendre des contacts, ...
 			</p>
 			<p>
 				En fin de matinée, l'équipe organisatrice et les intervenants de chaque entreprise seront conviés à un déjeuner de manière à ponctuer l'évènement par une pause conviviale.
@@ -55,8 +31,8 @@
 				Encore une fois, l'objectif de cet évènement est évidemment d'offrir une <span class="hero_motcle">visibilité unique</span> entre étudiants et entreprises, et de tisser des <strong>relations</strong> entre les deux.
 			</p>
 		</div>
-		<div id="infoRif" class="span8">
-			<h2>Informations</h2>
+		<div id="infoRif" class="span8" style="position:relative;">
+			<h2><i class="icon icon-search icon-white"></i> Informations</h2>
 
 			<h3>Date</h3>
 			<p>
@@ -65,14 +41,14 @@
 
 			<h3>Tarifs</h3>
 			<div class="row-fluid">
-				<div class="span6">
+				<div class="span7">
 					<ul>
 						<li>Entreprise parrainant l'une des trois promotions en cours : </li>
-						<li>Entreprise payant la taxe d'apprentissage à l'INSA : </li>
+						<li>Entreprise payant la taxe d'apprentissage au Département IF : </li>
 						<li>Autres entreprises : </li>
 					</ul>
 				</div>
-				<div class="span4">
+				<div class="span3">
 					<ul class="unstyled">
 						<li>Invitée</li>
 						<li>900€</li>
@@ -81,14 +57,14 @@
 				</div>
 			</div>
 			<p class="comment">
-				Ces tarifs sont uniquement à titre indécatifs, ceux-ci n'étant fixé qu'en Septembre.
+				Ces tarifs sont uniquement à titre indécatifs, ceux-ci n'étant fixés qu'en Septembre.
 			</p>
 			
 			<h3>Questions</h3>
 			<p>Pour plus d'informations concernant cet évènement, veuillez nous contacter à l'adresse suivante : <a href="#" e-name="aedi.entreprise.asso" class="e-noBot btn btn-mini btn-info" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
 			
 			<div id="inscriptionRif">
-				<h2>Inscription</h2>
+				<h2><i class="icon icon-pencil icon-white"></i> Inscription</h2>
 				<p>
 					Pour vous inscrire aux prochaines Rencontres IF, veuillez remplir le formulaire accessible via le lien ci-dessous. 
 				</p>
@@ -100,7 +76,4 @@
 			</div>
 		</div>
 	</div>
-	<script type="text/javascript">
-		$('#myCarousel, #myCarousel img').height($('#presentationRif').height() - 30);
-	</script>
 </div>
