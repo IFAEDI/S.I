364120de4274e733da4cc5c2862b6e6040fe16f3
Aldream <benjamin.planche@aldream.net>
2012-08-27 21:17:01 +0200

    Création Page de présentations pour entreprises
    Parrainage, Conf, Entretiens (Lorem Ipsum à remplacer)

---
+++ Serveur/web/parrainage/php/presentation.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * -----------------------------------------------------------
+ * Vue - PARRAINAGE, PRESENTATION
+ * -----------------------------------------------------------
+ * Auteur : Benjamin (Bill) Planche - Aldream (4IF 2011/12)
+ *          Contact - benjamin.planche@aldream.net
+ * ---------------------
+ * Page présentant du parrainage aux entreprises.
+ */
+?>
+
+<div id="parrainage" class="module_entr">
+	<div class="row">
+		<div class="span12">
+			<div class="hero-unit">
+				<h1>Parrainage</h1>
+				<p>
+					Le parrainage est un accord tri-parti entre une entreprise, le Département Informatique de l'INSA-Lyon, et l'AEDI, ayant pour objectif de tisser une relation étroite entre les étudiants et membres de l'entreprise, bénéfique pour tous.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="span4">
+			<h2><i class="icon icon-bookmark icon-white"></i> Concept</h2>
+			<p class="justify">
+				Chaque année, la nouvelle promotion entrante au Département Informatique (3 IF – Première année du cycle Ingénieur) à l’opportunité d’être parrainée par une entreprise, pour les trois années de ce second cycle.</p>
+			<p class="justify">
+				En échange <span class="hero_motcle">d'une présence et d'une visibilité assurées</span> au sein du Département et des évènements de l'AEDI, l’entreprise parrainant s’engage à co-organiser des évènements particuliers pour les étudiants (conférence de présentation, weekend d’intégration, ...).
+			</p>
+			<p class="justify">
+				
+Nulla et erat arcu. Ut vulputate, erat in blandit hendrerit, augue ante dignissim felis, et elementum tellus tellus nec nulla. Suspendisse faucibus nulla vitae tellus aliquam eget scelerisque neque ornare. Sed convallis lobortis mollis. Etiam in dolor vitae dolor molestie suscipit. Aliquam erat volutpat. Sed mi elit, egestas ut dignissim vulputate</span>, eleifend quis massa. Sed a purus ipsum. Etiam pulvinar tincidunt eros, ultrices dapibus dolor gravida ac.
+			</p>
+			
+			<h2><i class="icon icon-pencil icon-white"></i> Inscription</h2>
+			<p class="justify">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in erat a dui auctor eleifend bibendum et felis. Curabitur rhoncus scelerisque felis non vehicula. Suspendisse potenti. Maecenas velit dolor, porttitor sed tincidunt at, pharetra at lacus. Integer id sapien enim, interdum auctor ligula. In pulvinar nulla ut dolor ornare bibendum. Fusce et odio eleifend nisl placerat vehicula.
+			</p>
+
+			<p class="centre"><a href="index.php?page=Entretiens_Entreprise_Inscription" class="btn btn-large btn-primary"><strong><i class="icon-pencil icon-white"></i> Formulaire d'inscription</strong></a></p>
+			
+		</div>
+		<div id="infoRif" class="span8">
+			<p class="centre" style="margin-left:-10px; margin-bottom:20px;"><img class="width8" src="parrainage/img/thales2011.jpg" title="Rencontre entre les différents acteurs du parrainage 2011." alt="Rencontre entre les différents acteurs du parrainage 2011."></p>
+			
+			
+			<h2><i class="icon icon-search icon-white"></i> Informations</h2>
+
+			<p class="justify">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in erat a dui auctor eleifend bibendum et felis. Curabitur rhoncus scelerisque felis non vehicula. Suspendisse potenti. Maecenas velit dolor, porttitor sed tincidunt at, pharetra at lacus. Integer id sapien enim, interdum auctor ligula. In pulvinar nulla ut dolor ornare bibendum. Fusce et odio eleifend nisl placerat vehicula.
+			</p>
+			
+			<h3>Contact</h3>
+			<p>Pour plus d'informations concernant le parrainage, veuillez nous contacter à l'adresse suivante :</p>
+			<p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-mini btn-info" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
+
+			<h2><i class="icon icon-star icon-white"></i> Parrains actuels</h2>
+			<div id="infoRif" class="span8">
+				<div class="row">
+					<p class="span3">
+						<a href="http://www.altran.fr/" title="Altran - Parrain Promo 2013"><img class="width2" src="./commun/img/parrains_caroussel/altran.png" alt="Altran"></a>
+					</p>
+					<p class="span2">
+						<a href="http://www.axa.fr/" title="Axa - Parrain Promo 2015"><img class="width0-5 height1" src="./commun/img/parrains_caroussel/axa2.png" alt="Axa"></a>
+					</p>
+					<p class="span3">
+						<a href="http://www.thalesgroup.com/" title="Thalès - Parrain Promo 2014"><img class="width2" src="./commun/img/parrains_caroussel/thales.png" alt="Thalès"></a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<script>
+		// Image resizing :
+		$('.width8').attr('width', ($('#parrainage').width() / 12 * 8 - 30)+'px');
+		$('.width3').attr('width', ($('#parrainage').width() / 4)+'px');
+		$('.width2').attr('width', ($('#parrainage').width() / 6)+'px');
+		$('.width1-5').attr('width', ($('#parrainage').width() / 8)+'px');
+		$('.width1').attr('width', ($('#parrainage').width() / 10)+'px');
+		$('.width0-5').attr('width', ($('#parrainage').width() / 20)+'px');
+		
+		$('.height2').attr('height', ($('#parrainage').width() / 12)+'px');
+		$('.height1').attr('height', ($('#parrainage').width() / 20)+'px');
+	</script>
+</div>
