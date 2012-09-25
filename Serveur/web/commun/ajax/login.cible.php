f5686e31ca6c2608532a0ad221e43aae6bfafdab
PandiPanda <sebastien.meriot@insa-lyon.fr>
2012-09-25 11:54:27 +0200

    [Login] Factorisation du code + logging des actions

--- Serveur/web/commun/ajax/login.cible.php
+++ Serveur/web/commun/ajax/login.cible.php
@@ -22,9 +22,10 @@ if( @isset( $_GET['action'] ) ) {
 	if( $_GET['action'] == "regular_auth" ) {
 
 		if( !( @isset( $_GET['username'] ) && @isset( $_GET['password'] ) ) ) {
-			$val = array( "code" => "fail", "mesg" => "Variables manquantes." );
+			$val = genererReponseStdJSON( 'fail', 'Variables manquantes.' );
 		}
 		else {
+			$logger = Logger::getLogger("Login.RegularAuth");
 
 			$login = mysql_escape_string( $_GET['username'] );
 			$mdp   = mysql_escape_string( $_GET['password'] );
@@ -36,23 +37,26 @@ if( @isset( $_GET['action'] ) ) {
 				switch( $result ) {
 					/* Authentification réussie */
 					case Authentification::ERR_OK:
-						$val = array( "code" => "ok" );
+						$val = genererReponseStdJSON( 'ok', 'Authentification réussie.' );
+						$logger->info( "L'utilisateur \"".$login."\" s'est connecté. - ".$_SERVER['REMOTE_ADDR'] );
 						break;
 					/* Authentification foirée */
 					case Authentification::ERR_ID_INVALIDE:
-						$val = array( "code" => "fail", "mesg" => "Identifiants non valides." );
+						$val = genererReponseStdJSON( 'fail', 'Identifiants non valides.' );
 						break;
 					case Authentification::ERR_AMBIGUITE:
-						$val = array( "code" => "error", "mesg" => "Une ambiguité est survenue lors de la procédure d'authentification." );
+						$val = genererReponseStdJSON( 'error', 'Une ambiguité est survenue lors de la procédure d\'authentification.' );
+						$logger->warn( "Une ambiguité existe avec l'utilisateur \"".$login."\"." );
 						break;
 					case Authentification::ERR_BD:
-						$val = array( "code" => "error", "mesg" => "La base de données a rencontré une erreur. Veuillez réessayer ultérieurement." );
+						$val = genererReponseStdJSON( 'error', 'Une erreur interne survenue lors de la procédure d\'authentification. Veuillez réessayer ultèrieurement.' );
+						$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$login."\" a voulu se connecter." );
 						break;
 				}
 			}
 			catch( Exception $e ) {
-
-				$val = array( "code" => "error", "mesg" => $e->getMessage() );
+				$val = genererReponseStdJSON( 'error', $e->getMessage() );
+				$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$login."\" a voulu se connecter : ".$e->getMessage() );
 			}
 		}
 	}
@@ -66,9 +70,10 @@ if( @isset( $_GET['action'] ) ) {
 			if( !( @isset( $_GET['password'] ) && @isset( $_GET['nom'] ) && @isset( $_GET['prenom'] ) && @isset( $_GET['mails'] ) 
 				&& @isset( $_GET['telephones'] ) ) ) {
 
-				$val = array( "code" => "error", "mesg" => "Variables manquantes." );
+				$val = genererReponseStdJSON( 'error', 'Variables manquantes.' );
 			}
 			else {
+				$logger = Logger::getLogger("Login.UserInfoSave");
 
 				$utilisateur = $authentification->getUtilisateur();
 				$continue = true;
@@ -83,15 +88,15 @@ if( @isset( $_GET['action'] ) ) {
 					try {
 						$result = $utilisateur->changePassword( $password );
 						if( $result == false ) {
-
-							$val = array( "code" => "fail", "mesg" => "Une erreur est survenue lors de la modification du mot de passe." );
+							$val = genererReponseStdJSON( 'fail', 'Une erreur est survenue lors de la modification du mot de passe.');
+							$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur->getLogin()."\" a voulu changer son mdp." );
 							$continue = false;
 						}
 					}
 					catch( Exception $e ) {
 
-						$msg = "Une erreur est survenue lors de la requête à la base (".BD::getDerniereErreur().")";
-						$val = array( "code" => "error", "mesg" => $msg );
+						$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur>getLogin()."\" a voulu changer son mdp : ".$e->getMessage()." [Trace BD: ".BD::getDerniereErreur()."]" );
+						$val = genererReponseStdJSON( 'error', 'Une erreur est survenue lors de la modification du mot de passe.');
 						$continue = false;
 					}
 				}
@@ -108,35 +113,41 @@ if( @isset( $_GET['action'] ) ) {
 								$result = $utilisateur->getPersonne()->changeTelephones( $_GET['telephones'] );
 								if( $result ) {
 									$val = array( "code" => "ok", "nom" => $nom, "prenom" => $prenom );
+									$logger->info( "L'utilisateur \"".$utilisateur->getLogin()."\" a mis à jour ses infos." );
 								}
 								else {
-									$val = array( "code" => "fail", "mesg" => "Une erreur est survenue lors de la mise à jour des numéros de téléphone. Veuillez réessayer ultèrieurement." );
+									$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur->getLogin()."\" a voulu mettre à jour ses infos (téléphone)." );
+									$val = genererReponseStdJSON( 'fail', 'Une erreur est survenue lors de la modification des informations.');
 								}
 							}
 							else {
-								$val = array( "code" => "fail", "mesg" => "Une erreur est survenue lors de la mise à jour des mails. Veuillez réessayer ultèrieurement." );
+								$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur->getLogin()."\" a voulu mettre à jour ses infos (mail)." );
+								$val = genererReponseStdJSON( 'fail', 'Une erreur est survenue lors de la modification des informations.');
 							}
 						
 						}
 						else {
-							$val = array( "code" => "fail", "mesg" => "Une erreur est survenue lors de la mise à jour des infos. Veuillez réessayer ultèrieurement." );
+							$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur->getLogin()."\" a voulu mettre à jour ses infos (civilité)." );
+							$val = genererReponseStdJSON( 'fail', 'Une erreur est survenue lors de la modification des informations.');
 						}
 					}
 					catch( Exception $e ) {
 
-						$msg = "Une erreur est survenue lors de la requête à la base (".BD::getDerniereErreur().")";
-						$val = array( "code" => "fail", "mesg" => $msg );
+						$logger->error( "Une erreur est survenue lorsque l'utilisateur \"".$utilisateur->getLogin()."\" a voulu mettre à jour ses infos : ".$e->getMessage()."  / Trace BD : [".BD::getDerniereErreur()."]" );
+						$val = genererReponseStdJSON( 'fail', 'Une erreur est survenue lors de la modification des informations.');
 					}
 				}
 			}
 		}
 		else {
-
-			$val = array( "code" => "error", "mesg" => "Vous n'êtes pas authentifié." );
+			$logger->warn( "Requête d'un utilisateur non authentifié - ".$_SERVER['REMOTE_ADDR'] );
+			$val = genererReponseStdJSON( 'error', 'Vous n\'êtes pas authentifié.');
 		}
 	}
 	else {
-		$val = array( "code" => "error", "mesg" => "Action non autorisée." );
+		$logger = Logger::getLogger( "Login" );
+		$logger->warn( "Requête non valide [contrôleur inexistant] - ".$_SERVER['REMOTE_ADDR'] );
+		$val = genererReponseStdJSON( 'error', 'Action non autorisée.');
 	}
 
 	echo json_encode( $val );
