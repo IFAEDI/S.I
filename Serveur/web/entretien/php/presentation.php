75326650b6599465a7ff714d40228bee084ea6af
Benjamin Planche <benjamin.planche@aldream.net>
2013-02-08 23:29:07 +0900

    <MaJ> Sup. lien vers form Simu, Ajout message

--- Serveur/web/entretien/php/presentation.php
+++ Serveur/web/entretien/php/presentation.php
@@ -26,7 +26,7 @@
 			<p class="justify">
 				Les simulations d'entretiens ont lieu au département, hors temps scolaire. L'entreprise offre un certain nombre de créneaux durant lesquels les recruteurs dépêchés rencontreront les étudiants préalablement inscrits.</p>
 			<p class="justify">L'entretien se termine généralement par un échange entre l'étudiant et le professionnel, ce dernier pouvant revenir sur la prestation de l'élève, dresser son profil, prodiguer des conseils, ...</p>
-			<p class="justify">L'exercice est pris avec sérieux par les étudiants, pour qui il représente un entrainement efficace à cet épreuve subtile, et peu aussi parfois être l'occasion d'un premier échange avec une entreprise l'intéressant.</p>
+			<p class="justify">L'exercice est pris avec sérieux pour les étudiants, pour qui il représente un entrainement efficace à cet exercice subtil, et peu aussi parfois être l'occasion d'un premier échange avec une entreprise l'intéressant.</p>
 		</div>
 		<div id="infoRif" class="span8">
 			<h2><i class="icon icon-search icon-white"></i> Informations</h2>
@@ -48,8 +48,8 @@
 				L'AEDI servant d'intermédiaire entre les entreprises et le Département Informatique pour ce service, nous mettons à votre disposition un formulaire d'inscription que vous trouverez à l'adresse ci-dessous.<br/>
 				Une fois le formulaire remplis, celui-ci est transmis au Département qui vous donnera réponse dans les plus brefs délais.
 			</p>
-			<p class="centre"><a href="index.php?page=Entretiens_Entreprise_Inscription" class="btn btn-large btn-primary" disabled title="Service en cours de finition. Merci de consulter la page du Département pour vous inscrire en attendant : if.insa-lyon.fr/entreprise/simulation-entretiens. Merci de votre compréhension."><strong><i class="icon-pencil icon-white"></i> Formulaire d'inscription</strong></a></p>
-			<p style="color: #500; font-size: 0.9em;text-align:center;">Service en cours de finition. Merci de consulter la <a title="IF - Simulations d'entretiens" href="http://if.insa-lyon.fr/entreprise/simulation-entretiens" style="color: #500; text-decoration:underline;">page du Département</a> pour vous inscrire en attendant. Merci de votre compréhension.</p>
+			<p class="centre"><<?php /**a*/ ?>button <?php /**href="index.php?page=Entretiens_Entreprise_Inscription"*/ ?> class="btn btn-large btn-primary" disabled title="Service en cours de finition. Merci de consulter la page du Département pour vous inscrire en attendant : if.insa-lyon.fr/entreprise/simulation-entretiens. Merci de votre compréhension."><strong><i class="icon-pencil icon-white"></i> Formulaire d'inscription</strong></<?php /**a*/ ?>button></p>
+			<p class="alert alert-block alert-warning">Désolé, ce service est actuellent en cours de finition. Merci de consulter la <a title="IF - Simulations d'entretiens" href="http://if.insa-lyon.fr/entreprise/simulation-entretiens">page du Département</a> pour vous inscrire en attendant. Merci de votre compréhension.</p>
 		</div>
 	</div>
 </div>
