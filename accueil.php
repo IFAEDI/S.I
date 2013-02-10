ac91353dd1216eb01374955aa170484984bb21eb
Benjamin Planche <benjamin.planche@aldream.net>
2013-02-10 17:47:35 +0900

    <Responsive> Fix #20, Resize auto. bannière SG

--- accueil.php
+++ accueil.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 /**
  * -----------------------------------------------------------
  * Vue - ACCUEIL
@@ -79,7 +79,7 @@
 		  </div>
 		</div>
 	</div>
-	<div class="row" style="text-align:center;">
+	<div class="row" style="text-align:center;; margin-top:20px;">
 		<p class="span2">
 			<a href="http://www.insa-lyon.fr/" title="INSA-Lyon"><img class="adapt-width" span=2 scale=0.75 max=200 src="./commun/img/insa_logo.png" alt="INSA-Lyon"></a>
 		</p>
@@ -92,15 +92,15 @@
 		<p class="span2">
 			<a href="http://www.axa.fr/" title="Axa - Parrain Promo 2015"><img class="adapt-height adapt-width" span=2 scale=0.35 max=75 src="./commun/img/parrains_caroussel/axa2.png" alt="Axa"></a>
 		</p>
-		<p class="span3">
-			<a href="http://www.thalesgroup.com/" title="Thalès - Parrain Promo 2014"><img class="adapt-width" span=3 scale=0.75 max=200 src="./commun/img/parrains_caroussel/thales.png" alt="Thalès"></a>
-		</p>
-	</div>
-	<div class="row" style="text-align:center;">
-        <p class="span12">
-            <embed src="./commun/img/societe_generale.swf"  height="100" width="800" />
+        <p class="span3">
+            <a href="http://www.thalesgroup.com/" title="Thales - Parrain Promo 2014"><img class="adapt-width" span=3 scale=0.75 max=200 src="./commun/img/parrains_caroussel/thales.png" alt="Thales"></a>
         </p>
-	</div>
+        </div>
+        <div class="row" style="text-align:center;">
+            <p class="span12">
+                <embed style="margin-top:-20px;" src="./commun/img/societe_generale.swf" class="adapt-width" span=8 scale=0.60 max=800 />
+            </p>
+        </div>
 
 </div>
 	<div id="fb-root"></div>
