aa1001c0ebfb12869674da7d9f8aaf3fa8d78922
Benjamin Bouvier <bnjbouv+github@gmail.com>
2013-02-14 16:41:02 +0100

    Texte initial pour les conférences.

--- conferences/php/presentation.php
+++ conferences/php/presentation.php
@@ -4,6 +4,7 @@
  * Vue - CONFERENCES, PRESENTATION
  * -----------------------------------------------------------
  * Auteur : Benjamin (Bill) Planche - Aldream (4IF 2011/12)
+            Benjamin Bouvier - BenjBouv (4IF 2011/12) <benjamin.bouvier@gmail.com>
  *          Contact - benjamin.planche@aldream.net
  * ---------------------
  * Page présentant les conférences aux entreprises.
@@ -11,15 +12,22 @@
 ?>
 
 <div id="conf" class="module_entr">
-	<p class="alert alert-block alert-warning">Désolé, cette page est encore en cours d'élaboration. Merci de votre compréhension.</p>
-	<?php /**	
 	<div class="row">
 		<div class="span12">
 			<div class="hero-unit">
 				<h1>Conférences</h1>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in erat a dui auctor <strong>eleifend bibendum et felis</strong>. Curabitur rhoncus scelerisque felis non vehicula. Suspendisse potenti. Maecenas velit dolor, porttitor sed tincidunt at, pharetra at lacus. Integer id sapien enim, interdum auctor ligula. In pulvinar nulla ut dolor ornare bibendum. Fusce et odio eleifend nisl placerat vehicula.
-				</p>
+                Car le métier d'ingénieur en informatique évolue constamment au fil du temps, il est nécessaire
+                de savoir actualiser ses connaissances de manière active. En outre, le planning scolaire
+                ne permet pas de pouvoir aborder tous les sujets relatifs à l'ingénierie informatique. Pour ces raisons,
+                il est nécessaire que les étudiants apprennent par d'autres sources que l'école.
+                </p>
+                <p>
+                Pour leur faciliter la tâche, l'AEDI propose des conférences aux étudiants. Ces conférences sont
+                proposées par des ingénieurs issus du monde de l'entreprise, qui présentent des technologies ou des
+                concepts. Ces rencontres sont à la fois profitables pour les entreprises qui améliorent leurs carnets
+                de contacts et les étudiants qui en apprennent plus sur l'entreprise.
+                </p>
 			</div>
 		</div>
 	</div>
@@ -27,22 +35,44 @@
 		<div class="span4">
 			<h2><i class="icon icon-bookmark icon-white"></i> Concept</h2>
 			<p class="justify">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in erat a dui auctor <strong>eleifend bibendum et felis</strong>. Curabitur rhoncus scelerisque felis non vehicula. Suspendisse potenti. Maecenas velit dolor, porttitor sed tincidunt at, pharetra at lacus. Integer id sapien enim, interdum auctor ligula. In pulvinar nulla ut dolor ornare bibendum. Fusce et odio eleifend nisl placerat vehicula.
-			</p>
-			<p class="justify">
-				
-Nulla et erat arcu. Ut vulputate, erat in blandit hendrerit, augue ante dignissim felis, et elementum tellus tellus nec nulla. Suspendisse faucibus nulla vitae tellus aliquam eget scelerisque neque ornare. Sed convallis lobortis mollis. Etiam in dolor vitae dolor molestie suscipit. Aliquam erat volutpat. Sed mi elit, egestas ut <span class="hero_motcle">dignissim vulputate</span>, eleifend quis massa. Sed a purus ipsum. Etiam pulvinar tincidunt eros, ultrices dapibus dolor gravida ac.
+            Le concept des conférences est de faire se rencontrer les étudiants et les entreprises tout en permettant
+            futurs ingénieurs d'actualiser leurs connaissances du monde de l'entreprise. Pour cela, des employés d'une
+            entreprise viennent à l'INSA les jeudis après-midis, effectuent leurs présentations devant des étudiants,
+            inscrits de leur plein gré pour assister à la conférence. A la fin de la conférence, ingénieurs et étudiants
+            discutent de manière informelle, parfois autour d'un buffet. Suite à cette expérience, les parties prenantes
+            repartent avec de nouveaux contacts professionnels.
 			</p>
 		</div>
 		<div id="infoRif" class="span8">
 			<h2><i class="icon icon-search icon-white"></i> Informations</h2>
 
+            <p>
+            Vous êtes une entreprise et vous êtes intéressés pour présenter votre entreprise, une méthode, un thème
+            ou une technologie que vous maîtrisez en interne? Voici les informations qui vous intéressent.
+            </p>
+
 			<h3>Dates possibles</h3>
 			<p>
-				Les Jeudis après-midi, hors vacances scolaires.
+				Les Jeudis après-midi, hors périodes de vacances scolaires.
 			</p>
 			
 			<h3>Tarifs</h3>
+            <p>
+            Les tarifs pour présenter une conférence thématique dépendent du statut de votre entreprise par rapport
+            à l'AEDI: vous aurez ainsi accès à un tarif différent si votre entreprise parraine une des trois promotions
+            en cours, si votre entreprise paye la taxe d'apprentisssage au département IF ou si vous n'êtes dans aucune
+            de ces deux situations.
+            </p>
+            <p>
+            Ces tarifs comprennent les prestations suivantes:
+                <ul>
+                    <li>Communication auprès des étudiants (affichage, mailing, etc.).</li>
+                    <li>Logistique liée à la conférence (mise à disposition de la salle, etc.)</li>
+                    <li>etc.</li>
+                </ul>
+            </p>
+            <?php // TODO remplir les tarifs
+            /*
 			<h4>Conférences métiers</h4>
 			<div class="row-fluid">
 				<div class="span7">
@@ -81,19 +111,22 @@ Nulla et erat arcu. Ut vulputate, erat in blandit hendrerit, augue ante dignissi
 			<p>
 				Nulla et erat arcu. Ut vulputate, erat in blandit hendrerit, augue ante dignissim felis, et elementum tellus tellus nec nulla. 
 			</p>
+             */ ?>
 			
 			<h3>Questions</h3>
-			<p>Pour plus d'informations concernant cet évènement, veuillez nous contacter à l'adresse suivante :</p>
+			<p>Pour plus d'informations concernant l'organisation d'un tel évènement, n'hésitez pas à nous contacter à l'adresse suivante :</p>
 			<p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-mini btn-info" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
 
-			<h2><i class="icon icon-pencil icon-white"></i> Inscription</h2>
+            <?php // TODO partie en répétition de la partie précédente?
+            /*
+			<h2><i class="icon icon-pencil icon-white"></i>Inscription</h2>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in erat a dui auctor eleifend bibendum et felis. Curabitur rhoncus scelerisque felis non vehicula. Suspendisse potenti. Maecenas velit dolor, porttitor sed tincidunt at, pharetra at lacus. Integer id sapien enim, interdum auctor ligula. In pulvinar nulla ut dolor ornare bibendum. Fusce et odio eleifend nisl placerat vehicula.
 			</p>
 
-			<p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-large btn-primary" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i> Afficher Email</a></p>
+			<p class="centre"><a href="#" e-name="aedi.bureau" class="e-noBot btn btn-large btn-primary" e-domain="listes.insa-lyon.fr"><i class="icon-eye-open icon-white"></i>Afficher Email</a></p>
+             */ ?>
 			
 		</div>
 	</div>
-	*/ ?>
 </div>
